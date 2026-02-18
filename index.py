feb85541143471352689ff054590b2c1e951f541
taran3690 <tkaur3693@gmail.com>
2026-02-18 11:39:24 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,11 @@
+# A Variable is a name give to a memory location in a program.
+name="jassu"
+age=18
+price=25.254
+print(name)
+print(age)
+print(price)
+#name is variable. 
+#"jassu" is value.
+print("my name is : ",name) 
+print("my age is :",age) 
