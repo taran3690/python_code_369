ceaa159e6195550d7fd4964fcb73736a4bc76d00
taran3690 <tkaur3693@gmail.com>
2026-02-18 12:11:11 +0530

    Add files via upload

---
+++ project1.1.py
@@ -0,0 +1,20 @@
+#data types.integers,string,float,boolean,none.
+#integers=+ve,-ve,0.(25,-25,0).
+#string="jassu","hello".
+#float=3.99,2.55,5.99.
+#boolean=true/false.
+#none value .
+name="jassu"
+age=18
+price=25.254
+print(type(name))#str value
+print(type(age)) #int value
+print(type(price))#float value
+#boolean
+old=True
+even=False
+print(type(old)) 
+print(type(even))
+#none value
+a=None
+print(type(a))
