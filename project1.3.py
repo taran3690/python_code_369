1dacdf0ad83aa8809de811673214f00abd6256f5
taran3690 <tkaur3693@gmail.com>
2026-02-19 10:27:20 +0530

    Add files via upload

---
+++ project1.3.py
@@ -0,0 +1,54 @@
+#....chapter3.......
+#print sum
+a=5
+b=2
+sum=a+b
+print("a-b:",sum)
+#type of operators
+#an operator is a symbol that performs a certain operation between operands.
+# arithmetic operators(+,-,*,%,**)
+print("a-b:",a-b)
+print("a*b:",a*b)
+print("a%b:",a%b)#remainder
+print("a**b:",a**b)#a^b
+print("a/b:",a/b)
+#relation/comparison operators(==,!=,>,<,>=,<=)
+A=50
+B=20
+print("a==b:",A==B)#FALSE
+print("a!=b:",A!=B)#TRUE
+print("a>=b:",A>=b)#true
+print("a>b:",A>B)#true
+print("a<=b:",A<=B)#false
+print("a<b:",A<B) #false   
+#assignment operators(=,+=,*=,%=,**=)
+num0=10
+num0+=10   #20
+print("num0+=10:",num0)
+num=10
+num=num+10 #10+10=20
+print("num=num+10 :",num)
+num1=10
+num1*=5
+print("10*5 :",num1) #50
+num2=10
+num2/=5
+print("10/5 :",num2) #2.0
+num3=10
+num3%=5
+print("10%5 :",num3) #0
+num4=10
+num4**=5
+print("10**5 :",num4)  #1,00,000       
+ #logical operators(not,and,or)
+print( "not false:",not False)
+print("not true:",not True)   
+c=50
+d=30
+print(not (c>d))#50>30 true 
+val1=True
+val2=False
+print("and operator :",val1 and val2) # two value are true answer in true. but one value are false answer in false.           
+print("or operator :",val1 and val2)# one value are true answer in true.but two value are false answer in false. 
+print("or operator :",(c==d)or (c>d))
+
