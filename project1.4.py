c92d4eacdf74089ad6f74f33f99a1978adc040e1
taran3690 <tkaur3693@gmail.com>
2026-02-19 12:17:27 +0530

    Add files via upload

---
+++ project1.4.py
@@ -0,0 +1,22 @@
+#......chapter4........
+#type conversion
+a=2
+b=4.25
+sum=a+b #2.0+4.25=6.25
+print("2.0+4.25:",sum)
+#type
+c="2"
+d=4.25
+print(c)
+#error
+#e="2"
+#f=4.25
+#print(e+f)
+#casting
+g=int("2")
+h=4.25
+print(type(g))
+print("2.0+4.25",g+h)
+i=3.14
+j=str(a)
+print(type(b))
