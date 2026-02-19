30c369edfdd78ddcaecbb46738a413aa8c0f1500
taran3690 <tkaur3693@gmail.com>
2026-02-19 09:59:53 +0530

    Add files via upload

---
+++ project1.1.py
@@ -0,0 +1,13 @@
+# frist code in python.
+#........chapter 1...........
+# A Variable is a name give to a memory location in a program.
+name="hello world"
+age=18
+price=25.254
+print(name)
+print(age)
+print(price)
+#name is variable. 
+#"hello world" is value.
+print("my name is : ",name) 
+print("my age is :",age) 
