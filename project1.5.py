76540ca3cbb6bc9d0fa1b3e69abbb161c3b68a98
taran3690 <tkaur3693@gmail.com>
2026-02-21 12:11:44 +0530

    Add files via upload

---
+++ project1.5.py
@@ -0,0 +1,24 @@
+#.....chapter 5.......
+#from
+#name
+name=input("enter your name:")
+print("welcome",name)
+#age
+age=input("enter your age:")
+print("age",age)
+#marks
+marks=input("enter your marks:")
+print("marks:",marks)
+age1=int(input("enter age2:"))
+marks2=float(input("enter marks2:"))
+#str
+val=input("enter some value 0:")
+print(type(val),val)#all time in value str.
+#int
+int(input("first value1 :"))
+val1=int(input("enter some value :")) #first value
+print(type(val1),val1) #second value
+#float
+float(input("first value2:"))#first value
+val2=float(input("enter some value 2:")) #second value
+print(type(val2),val2)
