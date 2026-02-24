79d3161f1962865ab39096803544f62725e5a0b4
taran3690 <tkaur3693@gmail.com>
2026-02-24 12:16:04 +0530

    Add files via upload

---
+++ project1.6.py
@@ -0,0 +1,28 @@
+#......chapter6......
+#string and conditional statements.
+#strings.
+str1="his is string"
+str2='his is string'
+print(str1)
+print(str2)
+#concatenation.
+str3="hello"
+str4="world"
+print(str3+str4 )
+#escap sequre charactus.abs
+#tab,next line.ex \n,\t.
+str5="this is string.\n we are creating string"
+str6="this is string.\t we are creating string"
+print(str5)
+print(str6)
+#length of string.
+#len(str)
+str7="first string"
+len1=len(str7)
+print(len1)
+str8="second"
+len2=len(str8)
+print(len2)
+final_str=str7+""+str8
+print(final_str)
+print(len(final_str))
---
+++ project1.7.py
@@ -0,0 +1,28 @@
+#......chapter7....
+#indexing
+# H E L L O _ W O R  L D
+# 1 2 3 4 5 6 7 8 9 10 11
+#str= hello world
+#str[0] is 'A' str[1]is 'P'...
+#str[0]='B'#not allowed.
+str="hello world"
+ch=str[7]
+print(ch)
+print(str[1])
+#erorr
+#sre="hello world"
+#str[4]="@"
+#print(str)
+#slicing
+# accessing parts of strinh
+#str [starting_idx:ending_idx]#ending idex is not included
+#str="hello world"
+#str[1:4]is "ello"
+#str[ :4] is same as str[0:4]
+#str[1: ]is some as str[1: len(str)]
+print(str[6:11])#world
+print(str[6:len(str)])#world
+print(str[:4])#[0:4]
+print(str[5:])#[5:len(str)]
+
+ 
